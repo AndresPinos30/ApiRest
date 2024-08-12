ad48ada1f1c5c7128d474a623e432a3ae670f7ab
bryampinos <bryampinos30@gmail.com>
2024-08-11 20:57:05 -0500

    quinto commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
     UsersModule, 
     PassportModule,
     JwtModule.register({
-      secret: 'your-secret-key',
+      secret: 'kamaytech',
       signOptions: { expiresIn: '60m' },
     }),
   ],
